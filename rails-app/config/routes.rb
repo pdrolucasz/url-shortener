5bc019bb7a3481f126c99d572a44424022853713
pdrolucasz <pedro.lucas4431@gmail.com>
2025-10-31 01:49:38 -0300

    feat: add root route

--- rails-app/config/routes.rb
+++ rails-app/config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
     # Can be used by load balancers and uptime monitors to verify that the app is live.
     get "up" => "rails/health#show", as: :rails_health_check
 
-    # Defines the root path route ("/")
-    # root "home#index"
     namespace :api do
         resources :links, only: [ :index, :create, :destroy ]
     end
 
     get "/:short_code" => "redirect#show"
+
+    # Defines the root path route ("/")
+    # root "home#index"
+    root "rails/health#show"
 end
